a5611583d13631a9537ed4af7014e01a4b4b48cb
Pavel Byzov <pavel@byzov.com>
2011-09-13 11:52:27 +0700

    Happy day commit, add party date

--- templatetags/djangofeeds_tags.py
+++ templatetags/djangofeeds_tags.py
@@ -14,16 +14,25 @@ def three_days_news():
     cols = []
     today = date.today()
     start_date = False
-    days = (u'Сегодня', u'Вчера', u'Позавчера')
+    days = (u'Сегодня', u'Вчера был', u'')
+    party_days = (256,)
     for days_ago in range(3):
         end_date = today - timedelta(days=days_ago)
         if start_date:
             news = News.objects.filter(date__lt=start_date, date__gt=end_date)
         else:
             news = News.objects.filter(date__gt=end_date)
+        title = []
+        title.append(days[days_ago])
+        title.append(end_date.strftime('%j'))
+        title.append(u'день')
+        is_party_day = False
+        if int(end_date.strftime('%j')) in party_days:
+            is_party_day = True
         cols.append({
-            'date': days[days_ago], 
+            'date': ' '.join(title), 
             'news': news,
+            'is_party_day': is_party_day,
         })
         start_date = end_date
     t = template.loader.get_template('djangofeeds/tags/three_days_news.html')
